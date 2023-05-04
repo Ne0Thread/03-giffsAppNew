08de1e485cc75e6d4650082ab575c2c8080e47b5
Juan Cespedes <juankamilocromero@gmail.com>
2023-05-04 14:11:05 -0500

    Antes del child

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,17 +1,22 @@
-import { Component} from '@angular/core';
+import { Component, ElementRef, ViewChild} from '@angular/core';
 @Component({
   selector: 'gifs-search-box',
   template:`
   <h5>Buscar:</h5>
-  <input type="text" class="form-control" placeholder="Buscar gifs..." (keyup.enter)="searchTag(txtTagInput.value)" #txtTagInput>
+  <input type="text" class="form-control" placeholder="Buscar gifs..." (keyup.enter)="searchTag()" #txtTagInput>
 
   `
 })
 
 export class SearchBoxComponent{
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+  // el signo ! es para especificar a typescrip que siempre va a recibir el elemento, pues si no se coloca este va a sacar error por que puede ser null.
   constructor(){}
 
-  searchTag(newTag:string){
+  searchTag(){
+    const newTag =this.tagInput.nativeElement.value;
+
     console.log(newTag);
   }
 
